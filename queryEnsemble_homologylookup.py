1cf4851c40e0f3cad8b50a306a6e8b9c13e19d35
John Caskey <disulfidebond@gmail.com>
2017-03-27 14:58:05 -0500

    updated to accommodate NIH
    accommodates multiple gene listing, updated to accept 2 possible
    analyses: homology, and conversion to ENSEMBLE format

--- queryEnsemble_homologylookup.py
+++ queryEnsemble_homologylookup.py
@@ -13,39 +13,70 @@ def listOfGeneNames(l):
         for dline in inputdatafile:
             filedata.append(dline.rstrip())
     for i in filedata:
+        fileGenes = []
+        i = i.replace('"', '')
         isplit = i.split(',')
-        k = isplit[0]
-        v = isplit[1]
-        fileDict[k] = v
+        isplitItems = []
+        if len(isplit) != 2:
+            k = isplit[0]
+            splitDivider = len(isplit) / 2
+            for x in xrange(0, splitDivider):
+                isplitItems.append(isplit[x])
+            k += "#"
+            for val in isplitItems:
+                fileGenes.append(val)
+            fileDict[k] = fileGenes
+        else:
+            iSplitList = []
+            iSplitList.append(isplit[1])
+            k = isplit[0]
+            v = iSplitList
+            fileDict[k] = v
     return fileDict
-def lookupEnsembleID(ugeneID, ensQuery):
+def lookupEnsembleID(ugeneID, ensQuery, genomeName, verboseBool):
+    if verboseBool:
+        print("Checking")
+        print(ensQuery)
     uniqueID = ugeneID
     n = random.random() + 0.5
     time.sleep(n)
     server = "https://rest.ensembl.org/"
     lookupIDstring = "xrefs/symbol/"
-    speciesString = "armadillo/"
+    # speciesString = "armadillo/"
+    speciesString = genomeName + "/"
     idName = ensQuery
     ext1 = "?content-type=application/json"
     q = server+lookupIDstring+speciesString+idName+ext1
     r = requests.get(q, headers={ "Content-Type" : "application/json"})
     if not r.ok:
-        print q
-        print ensQuery+"\t"+"MATCH ERROR"
+        if verboseBool:
+            print(q)
+            print(ensQuery+"\t"+"MATCH ERROR")
+        else:
+            print("Error")
         return (uniqueID, (ensQuery, "MATCH ERROR"))
+        # rList.append([(uniqueID, (ensQuery, "MATCH ERROR"))])
         # r.raise_for_status()
         # sys.exit()
     else:
         decoded = r.json()
         if not decoded:
+            if verboseBool:
+                print("couldn't find match")
+            # rList.append([(uniqueID, (ensQuery, "NO MATCH"))])
             return (uniqueID, (ensQuery, "NO MATCH"))
         else:
             res = decoded[0]
             ensembleName = res["id"]
+            if verboseBool:
+                print("Found match for")
+                print(res)
+            # rList.append([(uniqueID, (ensQuery, ensembleName))])
             return (uniqueID, (ensQuery, ensembleName))
+    # return rList
 
-def checkGeneWithEnsemble(g_id, g):
-    geneResultCheck = lookupEnsembleID(g_id, g)
+def checkGeneWithEnsemble(g_id, g, genomeName, verboseBool):
+    geneResultCheck = lookupEnsembleID(g_id, g, genomeName, verboseBool)
     if geneResultCheck[1][1] == "MATCH ERROR":
         return 0
     elif geneResultCheck[1][1] == "NO MATCH":
@@ -56,65 +87,110 @@ def checkGeneWithEnsemble(g_id, g):
         else:
             return 0
 
-def lookupEnsembleHomology_NoSequence(ugeneID, ensQuery):
+def lookupEnsembleHomology_NoSequence(ugeneID, ensQuery, genomeName, verboseBool):
     uniqueID = ugeneID
-    noHomologyString = "NOMATCH_HOMOLOGY"
-    n = random.random() + 0.5
-    time.sleep(n)
-    server = "https://rest.ensembl.org/"
-    lookupIDstring = "homology/symbol/"
-    speciesString = "armadillo/"
-    idName = ensQuery
-    # ?content-type=application/json;format=condensed;target_species=human;type=orthologues
-    ext1 = "?content-type=application/json;format=condensed;"
-    targetSpecies = "target_species=human;"
-    ext2 = "type=orthologues"
-    q = server+lookupIDstring+speciesString+idName+ext1+targetSpecies+ext2
-    r = requests.get(q, headers={ "Content-Type" : "application/json"})
-    if not r.ok:
-        print ensQuery+"\t"+"MATCH ERROR OR NO MATCH"
-        return (uniqueID, (ensQuery, "MATCH_ERROR"))
-        # r.raise_for_status()
-        # sys.exit()
-    else:
-        decoded = r.json()
-        if not decoded:
-            return (uniqueID, (ensQuery, "NO MATCH"))
-        elif "error" in decoded:
-            return (uniqueID, (ensQuery, "NO MATCH")) # alternative output to above
+    returnItemsList = []
+    for itm in ensQuery:
+        noHomologyString = "NOMATCH_HOMOLOGY"
+        n = random.random() + 0.5
+        time.sleep(n)
+        server = "https://rest.ensembl.org/"
+        lookupIDstring = "homology/symbol/"
+        speciesString = genomeName + "/"
+
+        idName = itm
+        # ?content-type=application/json;format=condensed;target_species=human;type=orthologues
+        ext1 = "?content-type=application/json;format=condensed;"
+        targetSpecies = "target_species=human;"
+        ext2 = "type=orthologues"
+        q = server+lookupIDstring+speciesString+idName+ext1+targetSpecies+ext2
+        r = requests.get(q, headers={ "Content-Type" : "application/json"})
+        if not r.ok:
+            if verboseBool:
+                print itm+"\t"+"MATCH ERROR OR NO MATCH"
+            # return (uniqueID, (itm, "MATCH_ERROR"))
+            returnItemsList.append((uniqueID, (itm, "MATCH_ERROR")))
+            continue
         else:
-            res = ""
-            try: 
-                res = decoded["data"][0]["homologies"][0]["id"]
-            except IndexError:
-                res = noHomologyString
-            # optional: run error check here, error check
-            # re-queries with ID to verify gene ID matches Ensemble ID
-            # and throws error if it does not
-            # comment out to skip
-            ensQuery = decoded["data"][0]["id"]
-            errorCheck = checkGeneWithEnsemble(uniqueID, decoded["data"][0]["id"])
-            if not errorCheck:
-                print "####"
-                print "Warning! Gene name did not match ensemble check"
-                print decoded
-                print decoded["data"][0]["id"]
-                print res
-                print "####"
-            return (uniqueID, (ensQuery, res))
+            decoded = r.json()
+            if not decoded:
+                # return (uniqueID, (itm, "NO MATCH"))
+                returnItemsList.append((uniqueID, (itm, "NO MATCH")))
+                if verboseBool:
+                    print("No Match for")
+                    print(itm)
+                continue
+            elif "error" in decoded:
+                # return (uniqueID, (itm, "NO MATCH")) # alternative output to above
+                returnItemsList.append((uniqueID, (itm, "NO MATCH")))
+                if verboseBool:
+                    print("No Match for")
+                    print(itm)
+                continue
+            else:
+                res = ""
+                try:
+                    res = decoded["data"][0]["homologies"][0]["id"]
+                except IndexError:
+                    res = noHomologyString
+                # optional: run error check here, error check
+                # re-queries with ID to verify gene ID matches Ensemble ID
+                # and throws error if it does not
+                # comment out to skip
+                itm = decoded["data"][0]["id"]
+                errorCheck = checkGeneWithEnsemble(uniqueID, decoded["data"][0]["id"], genomeName, verboseBool)
+                if not errorCheck:
+                    if verboseBool:
+                        print "####"
+                        print "Warning! Gene name did not match ensemble check"
+                        print decoded
+                        print decoded["data"][0]["id"]
+                        print res
+                        print "####"
+                    else:
+                        print("Error with Ensemble Match")
+                # return (uniqueID, (itm, res))
+                returnItemsList.append((uniqueID, (itm, "NO MATCH")))
+                continue
+    return returnItemsList
 def main():
     parser = argparse.ArgumentParser(description='Parse Ensemble Data')
     parser.add_argument('-i', '--input', dest='input', help='Input gene list to query Ensemble REST database')
+    parser.add_argument('-g', '--genome', dest='genome', help='name for target genome to query')
+    parser.add_argument('-H', '--homology', dest='homology', help='homology lookup that target genome will be queried against')
+    parser.add_argument('-v', '--verbose', dest='verbose', help='verbose output, will provide all errors as a header, proceeded by the line \"###Results\"')
     args = parser.parse_args()
+    verboseBool = False
+    if args.verbose:
+        verboseBool = True
+    homologyLookup = False
+    if args.homology:
+        homologyLookup = True
+    genomeName = args.genome
+    if not genomeName:
+        print("Sorry, a genome name must be provided! Please provide one and rerun")
     mFileIn = args.input
+    if not mFileIn:
+        print("Sorry, an input file must be provided!  Please provide one and rerun")
     aDict = listOfGeneNames(mFileIn)
+    print(aDict)
     aResult = []
-    for k,v in aDict.items():
-        aResultItem = lookupEnsembleHomology_NoSequence(k,v)
-        aResult.append(aResultItem)
-    print "###" + "RESULTS"
-    for i in aResult:
-        s = i[0] + "," + i[1][0] + "," + i[1][1]
+    if homologyLookup:
+        for k,v in aDict.items():
+            aResultItem = lookupEnsembleHomology_NoSequence(k,v, genomeName, verboseBool)
+            aResult.append(aResultItem)
+    else:
+        for k,v in aDict.items():
+            vList = v
+            print(vList)
+            for vItem in vList:
+                aResultItem = lookupEnsembleID(k, vItem, genomeName, verboseBool)
+                aResult.append(aResultItem)
+    if args.verbose:
+        print("###Results")
+        print(aResult)
+    for itm in aResult:
+        s = itm[0] + "," + itm[1][0] + "," + itm[1][1]
         print s
 if __name__ == "__main__":
         main()
